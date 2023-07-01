a30291887db39090611b4cdeb6fd20f8633b638e
Marcelo Neris <marceloaneris@gmail.com>
2023-07-01 16:19:59 -0300

    Projeto finalizado

--- src/pages/MinhasPastas/MinhasPastasPage.js
+++ src/pages/MinhasPastas/MinhasPastasPage.js
@@ -1,19 +1,20 @@
 import Container from 'react-bootstrap/Container';
 import { ListGroup } from '../../components/ListGroup/ListGroup';
+import { useAppContext } from '../../store/AppContext';
+
+const adapterItems = (items) => {
+    return items.map(item => ({
+        title: item.name,
+        total: item.pins.length
+    }))
+}
 
 export const MinhasPastasPage = () => {
+    const {state} = useAppContext()
+
     return (
         <Container>
-            <ListGroup items={[
-                {
-                    title: 'Matemática',
-                    total: 3
-                },
-                {
-                    title: "JavaSAcript",
-                    total: 10
-                }
-            ]}/>
+            <ListGroup items = {adapterItems(state.folders)} />
         </Container>
     )
 }
